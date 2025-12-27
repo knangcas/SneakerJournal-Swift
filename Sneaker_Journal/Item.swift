f740a7fb5a9f4e88863c607bada9440d38c7d68a
knangcas <knangcas@asu.edu>
2025-12-27 07:29:02 -0600

    Initial Commit

---
+++ Sneaker_Journal/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Sneaker_Journal
+//
+//  Created by Kevin Nangcas on 12/27/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
