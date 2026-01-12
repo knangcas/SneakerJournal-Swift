2d45fc7f8d6cf32cd5cab65299b220e32258402f
knangcas <knangcas@asu.edu>
2026-01-11 20:01:33 -0600

    Refactored and added tab view

--- Sneaker_Journal/ContentView.swift
+++ Sneaker_Journal/ContentView.swift
@@ -9,57 +9,23 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-    @Query private var SneakerList : [Sneaker]
     
     
-
     var body: some View {
-        NavigationStack {
-            List {
-                ForEach(SneakerList) { item in
-                    NavigationLink {
-                        Text(item.brand)
-                    } label: {
-                        SneakerListView(sneaker:item)
-                    }
-                }
-                .onDelete(perform: deleteItems)
+        TabView {
+            Tab("List", systemImage: "list.clipboard") {
+                SneakerList()
+                    .toolbarBackgroundVisibility(.visible, for: .tabBar)
             }
-            .toolbar {
-                ToolbarItem (placement:.navigationBarLeading) {
-                    
-                }
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    NavigationLink {
-                        NewSneakerView()
-                    } label: {
-                            Label("Add Item", systemImage: "plus")
-                        
-                    }
-                }
-            }.navigationTitle("Sneaker List")
-        }
-    }
-    
-    
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(SneakerList[index])
+            Tab("Journal", systemImage: "square.and.pencil") {
+                Text("TODO")
+                    .toolbarBackgroundVisibility(.visible, for: .tabBar)
             }
+            Tab("Stats", systemImage: "chart.bar") {
+                Text("TODO")
+                    .toolbarBackgroundVisibility(.visible, for: .tabBar)
+            }
+            
         }
     }
 }
---
+++ Sneaker_Journal/SneakerList.swift
@@ -0,0 +1,65 @@
+//
+//  SneakerList.swift
+//  Sneaker_Journal
+//
+//  Created by Kevin Nangcas on 1/11/26.
+//
+
+import SwiftUI
+import SwiftData
+
+struct SneakerList: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query private var SneakerList : [Sneaker]
+    
+    
+
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(SneakerList) { item in
+                    NavigationLink {
+                        Text(item.brand)
+                    } label: {
+                        SneakerListView(sneaker:item)
+                    }
+                }
+                .onDelete(perform: deleteItems)
+            }
+            .toolbar {
+                ToolbarItem (placement:.navigationBarLeading) {
+                    //picker or sort sheet
+                    
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    EditButton()
+                }
+                ToolbarItem {
+                    NavigationLink {
+                        NewSneakerView()
+                    } label: {
+                            Label("Add Item", systemImage: "plus")
+                        
+                    }
+                }
+            }.navigationTitle("Sneaker List")
+        }
+    }
+    
+    
+
+    private func addItem() {
+        withAnimation {
+            let newItem = Item(timestamp: Date())
+            modelContext.insert(newItem)
+        }
+    }
+
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            for index in offsets {
+                modelContext.delete(SneakerList[index])
+            }
+        }
+    }
+}
